65ed22ec1fc0abe604f786fe10acfeaba498ba83
Sabalan Alaeiyeh <s.alaeiyeh@students.uu.nl>
2024-11-18 13:43:59 +0100

    added techcards with some tech, added profile picture

--- Portfolio/src/App.jsx
+++ Portfolio/src/App.jsx
@@ -2,9 +2,9 @@ import { useState } from 'react'
 import './App.css'
 import Card from './components/Card'
 import {NavBar} from "./components/NavBar.jsx";
+import {TechSection} from "./components/TechSection.jsx";
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
@@ -12,8 +12,10 @@ function App() {
         <div className="title">
             <h1>Welcome To Sab's Portfolio</h1>
         </div>
-        <div className="About">
+        <div className="about">
             <h2>About Me</h2>
+            <img id="profilePic" src="/src/assets/1704141184860.jpg" alt="Picture of me"></img>
+            <TechSection></TechSection>
         </div>
         <div className="cardsContainer">
             <Card title="Netflix Clone" description="For my first project with React, I decided to pick a website to clone. At this point I had read a lot of the React documentation and watched tutorials, I wanted to see how far I could get without having to design anything myself."></Card>
