7bc55d1931f6fda55dc08e2b399f7fac8b6125ea
Sabalan Alaeiyeh <s.alaeiyeh@students.uu.nl>
2024-11-20 16:24:31 +0100

    fixed some backgrounds having different colors on some browsers added see more button to title page

--- src/App.jsx
+++ src/App.jsx
@@ -8,18 +8,21 @@ function App() {
   return (
     <>
         <NavBar></NavBar>
-        <div className="title container">
+        <div className="title section">
             <h1>Hi, I'm Sabalan <br></br> I'm a front-end developer</h1>
+            <button>See More...</button>
         </div>
-        <div className="about container">
-            <div className="profile">
-                <img id="profilePic" src="/src/assets/1704141184860.jpg" alt="Picture of me"></img>
-                <p>Hi, I’m Sabalan, a front-end developer with an insatiable curiosity and a talent for mastering new skills. I thrive on learning—whether it’s learning new technologies, new languages or new design patterns.
-                    What sets me apart isn’t just my technical skills but my relentless drive to understand, improve, and grow. Every challenge is an opportunity to learn, and I dive headfirst into problem-solving with the excitement of discovering something new.</p>
+        <div className="aboutBackground section">
+            <div className="about">
+                <div className="profile">
+                    <img id="profilePic" src="/src/assets/1704141184860.jpg" alt="Picture of me"></img>
+                    <p>Hi, I’m Sabalan, a front-end developer with an insatiable curiosity and a talent for mastering new skills. I thrive on learning—whether it’s learning new technologies, new languages or new design patterns.
+                        What sets me apart isn’t just my technical skills but my relentless drive to understand, improve, and grow. Every challenge is an opportunity to learn, and I dive headfirst into problem-solving with the excitement of discovering something new.</p>
+                </div>
+                <TechSection></TechSection>
             </div>
-            <TechSection></TechSection>
         </div>
-        <div className="cardsContainer">
+        <div className="projects section">
             <Card title="Netflix Clone"
                   //description="For my first project with React, I decided to pick a website to clone. At this point I had read a lot of the React documentation and watched tutorials, I wanted to see how far I could get without having to design anything myself."
                 ></Card>
