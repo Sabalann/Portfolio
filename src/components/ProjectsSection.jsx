9ac4122c123c569e3870427fadf1c3a79e26fc08
Sabalan Alaeiyeh <s.alaeiyeh@students.uu.nl>
2024-12-08 17:48:37 +0100

    added Pacman project screenshot, added the Learning Programming App project

--- src/components/ProjectsSection.jsx
+++ src/components/ProjectsSection.jsx
@@ -1,7 +1,9 @@
 import React, { useEffect } from 'react';
 import Card from './Card.jsx';
 import EmblaCarousel from './EmbelaCarousel/EmblaCarousel.jsx';
-import netflixCloneSS from '../../public/netflixCloneSS.png'
+import netflixCloneSS from '../../public/projectScreenshots/netflixCloneSS.jpg'
+import pacmanSS from '../../public/projectScreenshots/pacmanSS.jpg'
+import programmingLearningSS from '../../public/projectScreenshots/programmingLearningSS.jpg'
 
 const OPTIONS = {}
 export function ProjectsSection() {
@@ -12,9 +14,16 @@ const SLIDES = [
             image={netflixCloneSS}
             liveLink="https://netflixclone.sabalan.info/" 
             repoLink="https://github.com/Sabalann/Netflix-Clone"/>,
-      <Card title="Pacman"></Card>
-
+      <Card 
+            title="Pacman"
+            image={pacmanSS}
+            repoLink="https://github.com/Sabalann/PacMan-Python"/>,
+      <Card
+            title="Learning Programming App"
+            image={programmingLearningSS}
+            repoLink="https://github.com/Sabalann/Programming-Learning-Windows-App"/>,
 ]
+
   return (
     <>
         <div>
